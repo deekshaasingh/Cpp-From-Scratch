8a12e2d31b0f5e63561c1a2bfeae4d3aa6851d83
Deeksha Singh <145819105+deekshaaah@users.noreply.github.com>
2024-02-29 20:33:11 +0530

    Add files via upload

---
+++ Folder/1. Basic C++ Syntax/Age as User Input.cpp
@@ -0,0 +1,13 @@
+// program to take age as a user input
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int age;
+	cout<<"Enter your age: ";
+	cin>>age;
+	cout<<"Your age is: "<<age;
+}
+
+
---
+++ Folder/1. Basic C++ Syntax/Roll Number.cpp
@@ -0,0 +1,11 @@
+// program to take user input
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int roll;
+	cout<<"Enter your Roll Number: ";
+	cin>>roll;
+	cout<<"You Roll number is "<<roll;
+}
