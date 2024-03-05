52b501a97aa852dba4148bfd6a65ed9a36da5bb9
Deeksha Singh <145819105+deekshaaah@users.noreply.github.com>
2024-03-05 20:03:18 +0530

    Add files via upload

---
+++ Folder/1. Basic C++ Syntax/Assignment.cpp
@@ -0,0 +1,31 @@
+// Question 1. Add a single and multi-line comment.
+
+// This is how you add a single line comment.
+/* This is how you add a 
+multi-line comment */
+
+/* Question 2. Which keyword is used to move the cursor to next line? 
+Write one C++ program to perform this task. */
+
+// endl
+/* #include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 344;
+	int b = 567;
+	cout<<a<<endl<<b;
+} */
+
+// How to take user input in C++? Write one C++ program to perform this task.
+
+/* #include <iostream>
+using namespace std;
+
+int main()
+{
+	string name;
+	cin>>name;
+	cout<<"Your name is "<<name;
+} */
