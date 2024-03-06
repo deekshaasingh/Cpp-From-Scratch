9e11daf084e59c5d1cdf5fba30e1750ea56e0c93
Deeksha Singh <145819105+deekshaaah@users.noreply.github.com>
2024-03-06 12:42:38 +0530

    Add files via upload

---
+++ Folder/3. Operators/Operators 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 6;
+	int b = 4;
+	
+	int sum = a+b;
+	int diff = a-b;
+	int prod = a*b;
+	int div = a/b;
+	
+	cout<<"Sum is "<<sum<<endl;
+	cout<<"Difference is "<<diff<<endl;
+	cout<<"Product is "<<prod<<endl;
+	cout<<"Division is "<<div;
+}
