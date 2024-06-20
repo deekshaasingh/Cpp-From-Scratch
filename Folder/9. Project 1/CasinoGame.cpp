4a69947b83525a03c852031d757c356e65d20acc
Deeksha Singh <145819105+deekshaaah@users.noreply.github.com>
2024-06-20 19:13:37 +0530

    Add files via upload

---
+++ Folder/9. Project 1/CasinoGame.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <cstdlib> //to generate random numbers
+#include <ctime>
+
+using namespace std;
+
+void rules()
+{
+	cout<<"CASINO NUMBER GUESSING RULES!"<<endl;
+}
+
