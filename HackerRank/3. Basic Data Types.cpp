d0eed20709ccf51c0d2c7948a2bc3cbd99a22372
Deeksha Singh <145819105+deekshaaah@users.noreply.github.com>
2024-06-21 19:19:10 +0530

    Create 3. Basic Data Types.cpp

---
+++ HackerRank/3. Basic Data Types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int d;
+    long ld;
+    char c;
+    float f;
+    double lf;
+    cin>>d>>ld>>c>>f>>lf;
+    cout<<d<<endl<<ld<<endl<<c<<endl;
+    cout<<fixed<<setprecision(3)<<f<<endl;
+    cout<<fixed<<setprecision(9)<<lf<<endl;
+    return 0;
+}
